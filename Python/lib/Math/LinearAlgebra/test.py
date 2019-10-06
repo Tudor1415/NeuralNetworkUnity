3fc402ca9b29d19dc1de324717bf1481bb5210b0
Tudor1415 <tudoropran2@gmail.com>
2019-10-06 18:19:23 +0200

    Update test.py

--- Python/lib/Math/LinearAlgebra/test.py
+++ Python/lib/Math/LinearAlgebra/test.py
@@ -2,12 +2,8 @@ from Matrix import Matrix
 
 matrix1 = Matrix(4, 5, initValue=1)
 matrix2 = Matrix(5, 5, initValue=2)
-# matrix3 = matrix1 * matrix2
-# print(matrix1)
-# print(matrix2)
-# print(matrix3)
+matrix3 = matrix1 * matrix2
+print(matrix1)
+print(matrix2)
+print(matrix3)
 
-for i in range(4):
-    for j in range(5):
-        matrix1.Content[i][j] = 3
-print(matrix1[1][4])
